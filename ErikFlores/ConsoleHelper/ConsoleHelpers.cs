96a1c1980e2e9113c8963ec8049a5fd41923db91
Erik <jerifkc@gmail.com>
2021-09-17 00:14:54 -0300

    En ConsoleHelper se agrego wrileRead, pinta el texto de consola en color rojo

--- ErikFlores/ConsoleHelper/ConsoleHelpers.cs
+++ ErikFlores/ConsoleHelper/ConsoleHelpers.cs
@@ -17,5 +17,19 @@ namespace ConsoleHelper
 				System.Console.ResetColor();
 			}
 		}
+
+		public static void WrileRed(string text)
+		{
+			try
+			{
+				System.Console.ForegroundColor = ConsoleColor.Red;
+				System.Console.BackgroundColor = ConsoleColor.Black;
+				System.Console.WriteLine(text);
+			}
+			finally
+			{
+				System.Console.ResetColor();
+			}
+		}
 	}
 }
