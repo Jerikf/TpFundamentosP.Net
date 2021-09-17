6cf210b1f74f22e71c39b933a66947fe3cbd182f
Erik <jerifkc@gmail.com>
2021-09-17 18:29:25 -0300

    Creacion del Ejercicio 3

---
+++ ErikFlores/Ejercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Realizar el algoritmo que permita el ingreso de 10 bolsas de alimento para
+               mascotas, con los kilos (validar entre 0 y 500) , sabor validar (carne vegetales pollo) e informar por consola:
+               a. El promedio de los kilos totales.
+               b. La bolsa más liviana y su sabor
+               c. La cantidad de bolsas sabor carne y el promedio de kilos de sabor carne
+              
+            */
+
+        }
+    }
+}
