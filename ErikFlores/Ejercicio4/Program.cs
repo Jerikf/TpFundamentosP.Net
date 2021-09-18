c43dce6a0418d6685f41fbfbb3e810d158bed081
Erik <jerifkc@gmail.com>
2021-09-18 09:10:13 -0300

    creación del Ejercicio4

---
+++ ErikFlores/Ejercicio4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*                  -----------------ENUNCIADO------------------------
+                Realizar el algoritmo que permita iterar el ingreso de dos datos de un vehiculo, un
+                color (rojo verde o amarillo) y un valor entre 0 y 10000 hasta que el usuario quiera e
+                informar al terminar el ingreso por consola:
+                a. La cantidad de rojos
+                b. La cantidad de rojos con precio mayor a 5000
+                c. La cantidad de vehículos con precio inferior a 5000
+                d. El promedio de todos los vehículos ingresados.
+                e. El más caro y su color. 
+              
+             */
+        }
+    }
+}
