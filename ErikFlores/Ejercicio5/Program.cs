d3331daf73b1e228c7bd9d8e68f4c0f8347a38e6
Erik <jerifkc@gmail.com>
2021-09-19 01:43:13 -0300

    Creacion del ejercicio5

---
+++ ErikFlores/Ejercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Realizar el algoritmo que permita ingresar el nombre de un estudiante, la edad
+            (validar) , el sexo (validar) y la nota del final (validar) hasta que el usuario quiera e
+            informar al terminar el ingreso por consola:
+            a. La cantidad de varones aprobados
+            b. El promedio de notas de los menores de edad
+            c. El promedio de notas de los adolescentes
+            d. El promedio de la notas de los mayores
+            e. El promedio de notas por sexo           
+            */
+
+        }
+    }
+}
